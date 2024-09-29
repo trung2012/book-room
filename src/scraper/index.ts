91261788109b86f968f3fe16434f3adca83af654
Trung <trungpham2012@gmail.com>
2024-09-29 12:44:51 -0700

    update

--- src/scraper/index.ts
+++ src/scraper/index.ts
@@ -1,31 +1,27 @@
 import { Page } from "@playwright/test";
 
 export const scraper = async (page: Page, day: number = 5) => {
-  try {
-    await page.goto('https://www.locurio.com/book/');
+  await page.goto("https://www.locurio.com/book/");
 
-    await page.waitForTimeout(5000);
+  await page.waitForTimeout(5000);
 
-    const iframe = page.frameLocator('#resova-iframe');
+  const iframe = page.frameLocator("#resova-iframe");
 
-    await iframe
-      .getByText(/Book now/i)
-      .first()
-      .click();
+  await iframe
+    .getByText(/Book now/i)
+    .first()
+    .click();
 
-    await iframe.locator('.filtersMenuDateSelect').click();
+  await iframe.locator(".filtersMenuDateSelect").click();
 
-    await iframe.locator('.nextmonth').nth(2).click();
+  await iframe.locator(".nextmonth").nth(2).click();
 
-    const calendar = iframe.locator('.resovaCalendarTimeSlotsOnly');
+  const calendar = iframe.locator(".resovaCalendarTimeSlotsOnly");
 
-    const row = calendar.locator('tbody').nth(2);
+  const row = calendar.locator("tbody").nth(2);
 
-    const cell = row.locator('td').nth(day - 1);
-    await cell.click();
+  const cell = row.locator("td").nth(day - 1);
+  await cell.click();
 
-    await iframe.getByText(/Continue/i).click();
-  } catch (err) {
-    console.error(err);
-  }
+  await iframe.getByText(/Continue/i).click();
 };
--- tests/scrape.spec.ts
+++ tests/scrape.spec.ts
@@ -1,14 +1,10 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { scraper } from "../src/scraper";
-
-const hypothesis = true;
 
 test("Scrape data", async ({ page }) => {
   await scraper(page, 5);
-  expect(hypothesis).toBe(true);
 });
 
 test("Scrape data 2", async ({ page }) => {
   await scraper(page, 6);
-  expect(hypothesis).toBe(true);
 });
