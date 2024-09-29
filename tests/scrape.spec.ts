022ae2edab7dba5b591fa9b7d6151328c78ccc03
Trung <trungpham2012@gmail.com>
2024-09-29 11:22:16 -0700

    update

--- tests/scrape.spec.ts
+++ tests/scrape.spec.ts
@@ -3,17 +3,12 @@ import { scraper } from "../src/scraper";
 
 const hypothesis = true;
 
-// Make sure tests run in serial mode, so if we write to a database, we know the order of operations
-test.describe.configure({ mode: "serial" });
-// Run indefinitely
-test.setTimeout(0);
-
 test("Scrape data", async ({ page }) => {
   await scraper(page, 5);
 
   setTimeout(async () => {
     await scraper(page, 6);
-  }, 30000)
+  }, 10000);
 
   expect(hypothesis).toBe(true);
 });
